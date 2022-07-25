e0694462cc60a8a5ffbcd28c4d5544a761592f86
dev <dev@localhost>
2022-07-25 05:31:04 +0000

    Add marshal method

--- internal/cache/redis.go
+++ internal/cache/redis.go
@@ -2,6 +2,8 @@ package cache
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
 	"github.com/spf13/viper"
 
@@ -10,6 +12,13 @@ import (
 
 type CacheService interface {
 	SaveNewTemplate(map[string]interface{}) error
+}
+
+type StrictTemplate struct {
+	BlockTemplateBlob string  `json:"blocktemplate_blob"`
+	Difficulty        float64 `json:"difficulty"`
+	SeedHash          string  `json:"seed_hash"`
+	Height            float64 `json:"height"`
 }
 
 type RedisService struct {
@@ -31,5 +40,20 @@ func NewClient() CacheService {
 
 func (r *RedisService) SaveNewTemplate(template map[string]interface{}) error {
 	key := "blocktemplate"
-	return r.client.Set(context.Background(), key, template, 0).Err()
+
+	redisTemplate := toRedisCompatibleStruct(template)
+	return r.client.Set(context.Background(), key, redisTemplate, 0).Err()
+}
+
+func toRedisCompatibleStruct(template map[string]interface{}) StrictTemplate {
+	return StrictTemplate{
+		BlockTemplateBlob: fmt.Sprintf("%v", template["blocktemplate_blob"]),
+		Difficulty:        template["difficulty"].(float64),
+		SeedHash:          fmt.Sprintf("%v", template["seed_hash"]),
+		Height:            template["height"].(float64),
+	}
+}
+
+func (s StrictTemplate) MarshalBinary() ([]byte, error) {
+	return json.Marshal(s)
 }
